7aeacf1b1fd036a89c5145c456245a1e29deb5c5
mplescano <mplescano@gmail.com>
2020-05-01 12:20:30 -0500

    Added a custom jwtconverter compatible to jdbc store.
    Fixed some unit test for the custom jwt converter.

---
+++ spring-security-oauth2/src/main/java/org/springframework/security/oauth2/provider/token/store/JwtJdbcTokenConverter.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2012-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.security.oauth2.provider.token.store;
+
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.security.jwt.JwtHelper;
+import org.springframework.security.oauth2.common.DefaultExpiringOAuth2RefreshToken;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.DefaultOAuth2RefreshToken;
+import org.springframework.security.oauth2.common.ExpiringOAuth2RefreshToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2RefreshToken;
+import org.springframework.security.oauth2.common.util.JsonParser;
+import org.springframework.security.oauth2.common.util.JsonParserFactory;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+
+/**
+ * Helper that improves the defatul JWT converter. Also acts as a {@link TokenEnhancer} when tokens are
+ * granted.
+ *
+ * <p>
+ * @deprecated See the <a href="https://github.com/spring-projects/spring-security/wiki/OAuth-2.0-Migration-Guide">OAuth 2.0 Migration Guide</a> for Spring Security 5.
+ *
+ * @see TokenEnhancer
+ * @see AccessTokenConverter
+ *
+ * @author Dave Syer
+ * @author Luke Taylor
+ * @author Mack Lescano
+ */
+@Deprecated
+public class JwtJdbcTokenConverter extends JwtAccessTokenConverter {
+
+    public static final String REFRESH_TOKEN_ID = "rti";
+    
+    public static final String TOKEN_TYPE = "token_type";
+    
+    private JsonParser objectMapper = JsonParserFactory.create();
+
+    public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+        DefaultOAuth2AccessToken result = new DefaultOAuth2AccessToken(accessToken);
+        Map<String, Object> info = new LinkedHashMap<String, Object>(accessToken.getAdditionalInformation());
+        String tokenId = result.getValue();
+        if (!info.containsKey(TOKEN_ID)) {
+            info.put(TOKEN_ID, tokenId);
+        }
+        else {
+            tokenId = (String) info.get(TOKEN_ID);
+        }
+        result.setAdditionalInformation(info);
+        OAuth2RefreshToken refreshToken = result.getRefreshToken();
+        if (refreshToken != null) {
+            DefaultOAuth2AccessToken encodedRefreshToken = new DefaultOAuth2AccessToken(accessToken);
+            encodedRefreshToken.setValue(refreshToken.getValue());
+            // Refresh tokens do not expire unless explicitly of the right type
+            encodedRefreshToken.setExpiration(null);
+            try {
+                Map<String, Object> claims = objectMapper
+                        .parseMap(JwtHelper.decode(refreshToken.getValue()).getClaims());
+                if (claims.containsKey(TOKEN_ID)) {
+                    encodedRefreshToken.setValue(claims.get(TOKEN_ID).toString());
+                }
+            }
+            catch (IllegalArgumentException e) {
+            }
+            Map<String, Object> refreshTokenInfo = new LinkedHashMap<String, Object>(
+                    accessToken.getAdditionalInformation());
+            info.put(REFRESH_TOKEN_ID, encodedRefreshToken.getValue());
+            result.setAdditionalInformation(info);
+            refreshTokenInfo.put(TOKEN_ID, encodedRefreshToken.getValue());
+            refreshTokenInfo.put(TOKEN_TYPE, "refresh_token");
+            encodedRefreshToken.setAdditionalInformation(refreshTokenInfo);
+            DefaultOAuth2RefreshToken token = new DefaultOAuth2RefreshToken(
+                    encode(encodedRefreshToken, authentication));
+            if (refreshToken instanceof ExpiringOAuth2RefreshToken) {
+                Date expiration = ((ExpiringOAuth2RefreshToken) refreshToken).getExpiration();
+                encodedRefreshToken.setExpiration(expiration);
+                token = new DefaultExpiringOAuth2RefreshToken(encode(encodedRefreshToken, authentication), expiration);
+            }
+            result.setRefreshToken(token);
+            result.setValue(encode(result, authentication));
+        }
+        else {
+            result.setValue(encode(result, authentication));
+        }
+        return result;
+    }
+
+    public boolean isRefreshToken(OAuth2AccessToken token) {
+        return token.getAdditionalInformation().containsKey(TOKEN_TYPE);
+    }
+}
